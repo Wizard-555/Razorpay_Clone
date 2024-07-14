3800d973b3ff6228266a459c9a31b58fe729fb13
Wizard-555 <kishankishore55555@gmail.com>
2024-07-14 20:46:54 +0530

    fifth commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        mullish : ["Mullish", "sans-serif"],
+      },
+      colors: {
+        deepblue: "#02042a",
+        lightBlue: "#2b84ea",
+        lightBlue500: "#4b94ed",
+        lightBlue300: "#0b72e7",
+        greenLight: "#61cea6",
+        grayText: "#818597",
+        grayBlue: "#6699cc",
+        deepBlue: "344a6c",
+        deepBlueHead: "#162f56",
+        gray2: "#525a76"
+      },
+    },
+  },
+  plugins: [],
+}
